99685ed1aec71456adc6f25a1a1599dfde6f37b3
Andre Gagnon <ajgagnon@uwalumni.com>
2025-11-04 16:02:40 -0600

    Update AdminPageMiddleware to enqueue admin-specific JavaScript and CSS assets

--- app/src/Middleware/AdminPageMiddleware.php
+++ app/src/Middleware/AdminPageMiddleware.php
@@ -11,29 +11,29 @@ use AndreBaseCore\Requests\RequestInterface;
 class AdminPageMiddleware {
 
 	/**
-	 * Enqueue component assets.
+	 * Handle admin page middleware.
 	 *
 	 * @param RequestInterface $request Request.
 	 * @param Closure          $next Next.
 	 * @return function
 	 */
 	public function handle( RequestInterface $request, Closure $next ) {
-		// Enqueue scripts.
-		$dashboard_asset_file = include trailingslashit( \AndreBase\AndreBase::core()->assets()->getPath() ) . 'build/index.asset.php';
+		// Enqueue JavaScript.
+		$dashboard_asset_file = include trailingslashit( \AndreBase\AndreBase::core()->assets()->getPath() ) . 'build/admin.asset.php';
 		wp_enqueue_script(
 			'andre-base-admin',
-			trailingslashit( \AndreBase\AndreBase::core()->assets()->getUrl() ) . 'build/index.js',
+			trailingslashit( \AndreBase\AndreBase::core()->assets()->getUrl() ) . 'build/admin.js',
 			$dashboard_asset_file['dependencies'],
 			$dashboard_asset_file['version'],
 			true
 		);
 
-		$dashboard_style_asset_file = include trailingslashit( \AndreBase\AndreBase::core()->assets()->getPath() ) . 'build/admin.scss.asset.php';
+		// Enqueue CSS.
 		wp_enqueue_style(
 			'andre-base-admin-style',
-			trailingslashit( \AndreBase\AndreBase::core()->assets()->getUrl() ) . 'build/index.css',
-			$dashboard_style_asset_file['dependencies'],
-			$dashboard_style_asset_file['version']
+			trailingslashit( \AndreBase\AndreBase::core()->assets()->getUrl() ) . 'build/admin.css',
+			array(),
+			$dashboard_asset_file['version']
 		);
 
 		return $next( $request );
